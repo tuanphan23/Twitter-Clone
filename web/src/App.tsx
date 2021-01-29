080760e435734e53300e6c5ad7d099f5707a93db
Tuan Phan <tphan@seattleu.edu>
2021-01-29 14:11:49 -0800

    Added Authentication, Formik for Form, Yup for validation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,33 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import React from "react";
 import "./App.css";
 import Users from "./components/Users";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
+import { setContext } from "apollo-link-context";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+const authLink = setContext(async (req, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    ...headers,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const link = authLink.concat(httpLink as any);
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  link: link as any,
   cache: new InMemoryCache(),
 });
 
@@ -17,6 +38,12 @@ function App() {
         <Switch>
           <Route path="/landing">
             <Landing />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
           <Route path="/">
             <Users />
