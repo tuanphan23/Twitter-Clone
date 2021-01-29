50a3e31df26498ed846067b886f3f0399242eb43
Tuan Phan <tphan@seattleu.edu>
2021-01-29 15:07:46 -0800

    Added protected route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,6 +12,7 @@ import Landing from "./components/Landing";
 import { setContext } from "apollo-link-context";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import IsAuthenticated from "./components/IsAuthenticated";
 
 const httpLink = new HttpLink({ uri: "http://localhost:4000" });
 const authLink = setContext(async (req, { headers }) => {
@@ -45,9 +46,11 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
-          <Route path="/">
-            <Users />
-          </Route>
+          <IsAuthenticated>
+            <Route path="/users">
+              <Users />
+            </Route>
+          </IsAuthenticated>
         </Switch>
       </Router>
     </ApolloProvider>
